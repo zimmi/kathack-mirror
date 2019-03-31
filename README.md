2ff1c91ed98f6ac0e3b069b6b8da33dfe8c128fd
thomas <zimmi@users.noreply.github.com>
2019-03-31 13:48:03 +0200

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# kathack-mirror
-Unofficial mirror of http://kathack.com
+# What?
+
+This is an unofficial mirror of the excellent [http://kathack.com](http://kathack.com) created by Alex Leone, David Nufer and David Truong.
+More specifically, the files making up the bookmarklet itself were copied and modified to load from this github repo.
+
+# Why?
+
+The original bookmarklet doesn't support sites using HTTPS, which now is practically all of them.
+
+# Credit
+
+All Credit goes to the original authors!
+
+
+Copyright Alex Leone, David Nufer, David Truong, 2011-03-11. kathack.com
--- docs/kh.js
+++ docs/kh.js
@@ -1,7 +1,8 @@
 /*
 Copyright Alex Leone, David Nufer, David Truong, 2011-03-11. kathack.com
+Modified by Thomas Zimmermann, 2019-03-31: Mirror files to work on sites using HTTPS.
 
-javascript:var i,s,ss=['http://kathack.com/js/kh.js','http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js'];for(i=0;i!=ss.length;i++){s=document.createElement('script');s.src=ss[i];document.body.appendChild(s);}void(0);
+javascript:var i,s,ss=['https://zimmi.github.io/kathack-mirror/ks.js','https://code.jquery.com/jquery-1.5.1.min.js'];for(i=0;i!=ss.length;i++){s=document.createElement('script');s.src=ss[i];document.body.appendChild(s);}void(0);
 
 */
 var BORDER_STYLE = "1px solid #bbb",
@@ -726,7 +727,7 @@ function whenAllLoaded(gameDiv, popup, stickyNodes) {
                 bgmusic = document.createElement('audio');
                 bgmusic.id = 'khbgmusic';
                 bgmusic.loop = 'loop';
-                bgmusic.src = 'http://kathack.com/js/katamari.mp3';
+                bgmusic.src = 'https://zimmi.github.io/kathack-mirror/katamari.mp3';
                 gameDiv.appendChild(bgmusic);
             }
             bgmusic.play();
@@ -762,6 +763,7 @@ z-index:501;';
     d.innerHTML = '<h1 style="font-size:16pt">\
 <a href="http://kathack.com/" style="color:blue;text-decoration:none;">\
 Katamari!</a></h1>\
+<p><small>Mirrored for HTTPS <a href="https://github.com/zimmi/kathack-mirror">here</a>.</small></p>\
 <button style="position:absolute;top:0;right:0;">X</button>\
 <p>Controls: Hold down <b><select id="mouseb">\
 <option value="0">Left-Click</option>\
